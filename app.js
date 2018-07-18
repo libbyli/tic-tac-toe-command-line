a3625f8a9790991ea733ce8913f88e1ec3213a99
Elizabeth Li <sincerelylibby@gmail.com>
2018-07-18 14:17:22 -0700

    Stub out game methods.

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-const TicTacToe = () => {
-  this.board = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+class TicTacToe {
+  constructor() {
+    this.board = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+    this.player = 'x';
+    this.moves = 0;
+  }
+
+  move(row, column) {
+    this.board[row][column] = this.player;
+  }
+
+  changePlayer() {
+    if (this.player === 'x') {
+      this.player = 'o';
+    } else {
+      this.player = 'x';
+    }
+  }
+
+  checkValidMove() {
+    // check to see if move is valid
+      // can't be on already occupied space
+      // has to be on the board
+  }
+
+  checkWins() {
+    // after 4 moves, check if move caused player to win
+  }
+
+  start() {
+    console.log(`${this.board}`);
+  }
 }
+
+const game = new TicTacToe();
+game.start();
